ed43710fd4b10deedcd6e3dda7eb564e03bae742
danji90 <danji_ma90@hotmail.com>
2018-07-11 15:47:27 +0200

    copies input shapefile, still have to loop-delete fields using SQL

--- preprocessing.py
+++ preprocessing.py
@@ -1,39 +1,35 @@
 from sys import path
 import os
 import ogr
-from math import hypot
-from datetime import datetime
 
-in_path = "D:\\Bulk\\Uni\\Muenster\\Python_for_GIS\\FinalProject\\movebank\\eagle_owl\\owlTest\\points.shp"
+source_path = "D:\\Bulk\\Uni\\Muenster\\Python_for_GIS\\FinalProject\\movebank\\eagle_owl\\owlTest\\"
+in_name = "points.shp"
+out_name = "outputPoints.shp"
+
+in_path = source_path + in_name
 
 print(in_path)
-#T  This function excludes owls that will not be considered by our core algorithm (eg: due to data incompleteness)
+
 def selectFields(shp):
     driver = ogr.GetDriverByName("ESRI Shapefile")
-    data_source = driver.Open(shp, 1)
-    layer = data_source.GetLayer(0)
+    data_source = driver.Open(shp, 0)
+    # layer = data_source.GetLayer(0)
 
-    print(layer)
+    out_path = source_path + out_name
+    shpCopy = driver.CopyDataSource(data_source, out_path)
 
-    # features = [feat for feat in layer.getFeatures()]
-    fields = layer.fields()
-    print(fields)
+    new_source = driver.Open(shpCopy, 1)
+    layer = shpCopy.GetLayer(0)
 
-    # Check for editing rights (capabilities)
-    # caps = layer.dataProvider().capabilities()
-    # print(caps)
-    # caps_string = layer.dataProvider().capabilitiesString()
-    # print(caps_string)
-    #
-    # # Now remove the fields again because we do not need them
-    # # We need the field index
-    # # Unfortunately backward counting (-1 and -2) does not work
-    # if caps & QgsVectorDataProvider.DeleteAttributes:
-    #     res = layer.dataProvider().deleteAttributes([3,4,5,6,9,11,12,16,17,18,19,22,25,26,27])
-    #
-    # # update to propagate the changes
-    # layer.updateFields()
+    feat = layer.GetFea
 
+    dropFields = [3,4,5,6,9,11,12,16,17,18,19,22,25,26,27]
+
+    for
+
+    ds.ExecuteSQL("ALTER TABLE my_shp DROP COLUMN my_field")
+
+    del shpCopy
 
 selectFields(in_path)
 # print(output)
